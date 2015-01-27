7cfd464de7901f4701580479e197ea91508b53ed
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-27 16:55:24 -0700

    finished

--- directives/clockWeather.js
+++ directives/clockWeather.js
@@ -1,15 +1,24 @@
 'use strict';
 var app = angular.module('weatherApp');
 
-app.directive('clockWeather', function() {
+app.directive('clockWeather', function($interval) {
 	return {
-		template: '<div>{{hours}}:{{minutes}}:{{seconds}}</div>',
+		scope: {
+			message: '@'
+		},
+		template: '<div>{{hours}}:{{minutes}}:{{seconds}}</div><p>{{message}}</p>',
 		restrict: 'E',
-		link: function(scope, elmnt, attrs) {
+		link: function(scope, elem, attrs) {
 			var date = new Date();
 			scope.hours = date.getHours();
 			scope.minutes = date.getMinutes();
 			scope.seconds = date.getSeconds();
+			$interval(function() {
+				var date = new Date();
+				scope.hours = date.getHours();
+				scope.minutes = date.getMinutes();
+				scope.seconds = date.getSeconds();
+			}, 1000);
 		}
-	}
+	};
 });
